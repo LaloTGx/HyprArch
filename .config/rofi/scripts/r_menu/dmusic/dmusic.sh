2a1779067807533e9ef0e47edf3b4b945323be8b
LaloTGx <89763322+LaloTGx@users.noreply.github.com>
2026-01-02 20:35:24 -0600

    Update rofi script dmusic
    Actualice el script ahora tiene un titulo como storage y ahora agregue
    una salida presionando [enter] sin tener nada en el campo de la url de
    youtube y le cambie el idioma al ingles. :)

--- .config/rofi/scripts/r_menu/dmusic/dmusic.sh
+++ .config/rofi/scripts/r_menu/dmusic/dmusic.sh
@@ -1,16 +1,44 @@
 #!/bin/bash
 
+# Colores para texto
+RED="\033[1;31m"
+REDLIGHT="\e[1;91m"
+YELLOW="\e[1;93m"
+CYANLIGHT="\e[0;96m"
+WHITELIGHT="\e[1;97m"
+BGBLACK="\e[40m"
+RESET="\033[0m"
+
+# Ruta base
+TITLE_DIR="$HOME/.config/rofi/scripts/r_menu/dmusic"
+
+# Mostrar título
+show_title() {
+  clear
+  echo -e "${CYANLIGHT}"
+  cat "$TITLE_DIR/titledmusic.txt"
+  echo -e "${YELLOW}"
+}
+
 TARGET_DIR="$HOME/Music"
 
 if [ ! -d "$TARGET_DIR" ]; then
     mkdir -p "$TARGET_DIR"
 fi
 
-read -p "Introduce la URL de YouTube: " url
-echo "Selecciona el formato:"
+show_title
+echo
+echo
+echo " Press [Enter] without a URL to exit"
+echo
+echo -e "${RESET}"
+read -p "  Paste YouTube URL: " url
+echo
+[[ -z "$url" ]] && exit 0
+echo "Select download format music:"
 echo "1) mp3"
 echo "2) flac"
-read -p "Opción [1-2]: " choice
+read -p "Option [1-2]: " choice
 
 case $choice in
     1)
@@ -20,7 +48,7 @@ case $choice in
         ext="flac"
         ;;
     *)
-        echo "Opción no válida."
+        echo "Invalid option. Exiting."
         exit 1
         ;;
 esac
