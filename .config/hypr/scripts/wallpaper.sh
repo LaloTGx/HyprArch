2cab123eb637acf75ac6d2b4c72da951784544c3
LaloTGx <89763322+LaloTGx@users.noreply.github.com>
2026-03-20 17:52:58 -0600

    Update Hyprland and Add scripts wallpaper
    Cambie una linea de hyprland para que tras iniciar no solo ejecute swww
    para mostrar la imagen, ahora apunta a un script dedicado al inicio de
    wallpaper donde aparte de escoger la imagen de fondo (principalmente en
    modo  claro en el caso de no obtenerlo pasara al modo oscuro) tambien
    utiliza matugen para obtener la paletas de colores del wallpaper para
    aplicarlo a la aplicaciones que agregaste en el template. :)

---
+++ .config/hypr/scripts/wallpaper.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Rutas
+BG_DIR="$HOME/.config/hypr/bg"
+DARK_DIR="$BG_DIR/bgdark"
+LIGHT_DIR="$BG_DIR/bglight"
+
+# Buscamos una imagen aleatoria dentro del directorio de fondos
+WALLPAPER=$(find "$BG_DIR" -type f \( -name "*.jpg" -o -name "*.png" -o -name "*.jpeg" -o -name "*.webp" \) | shuf -n 1)
+
+# Si por alguna razón no encuentra nada, salimos para evitar errores
+if [ -z "$WALLPAPER" ]; then
+    exit 1
+fi
+
+# Lógica de detección de carpeta para el MODO
+if [[ "$WALLPAPER" == *"/bglight/"* ]]; then
+    MODE="light"
+elif [[ "$WALLPAPER" == *"/bgdark/"* ]]; then
+    MODE="dark"
+else
+    MODE="dark"
+fi
+
+# Aplicar el wallpaper con swww
+swww img "$WALLPAPER" --transition-type wipe --transition-step 90
+
+# Ejecutar matugen con el modo detectado
+matugen image "$WALLPAPER" --mode "$MODE" --source-color-index 0 --continue-on-error -j hex > "$HOME/.cache/matugen/colors.json"
